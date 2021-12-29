fde899757160ba8fbcba886f3d6f307dd4c13bb8
Domain314 <artjom.moisejev@hotmail.com>
2021-12-29 13:40:23 +0100

    PHP Start: home
    converted HTML version of the home screen, to PHP.
    Added some structure to folder hierarchy

---
+++ htdocs/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <?php include "php/head.php" ?>
+  <body>
+
+    <?php
+      // session_start()
+      // $_GET["site"]
+     ?>
+
+     <?php include "php/utils/navbar.php" ?>
+     <?php include "php/utils/BG-Banner.php" ?>
+
+     <?php include "php/site/home.php" ?>
+
+
+
+     <!-- Bootstrap Bundle with Popper -->
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
+</html>
---
+++ htdocs/php/utils/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+  <div class="container">
+    <a class="qd-btn-hover px-3 logo" href="../index.html">
+      <strong>QiÐom</strong>
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a href="../news.html" class="nav-link qd-btn-hover">News</a>
+        </li>
+        <li class="nav-item">
+          <a href="../impressum.html" class="nav-link qd-btn-hover">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a href="../login.html" class="nav-link qd-btn-hover">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
