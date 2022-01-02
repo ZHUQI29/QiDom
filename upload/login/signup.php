08478c0d27062ebb66f35c631ba12a281e235514
ZHUQI29 <92386682+ZHUQI29@users.noreply.github.com>
2022-01-02 06:08:07 +0100

    new

--- upload/login/signup.php
+++ upload/login/signup.php
@@ -4,11 +4,24 @@
     if(!isset($_POST['submit'])){
         exit("Wrong execution");
     }//判断是否有submit操作 Determine whether there is a submit operation
+    $employed=$_POST['employed'];
+    $anrede=$_POST['anrede'];
+
+    $Vname=$_POST['Vname'];
+    $Nname=$_POST['Nname'];
+    $PLZ=$_POST['PLZ'];
+    $Ort=$_POST['Ort'];
+    $Straße=$_POST['Straße'];
+    $Hausnummer=$_POST['Hausnummer'];
+    $birthday=$_POST['birthday'];
+    $Email=$_POST['Email'];
 
     $name=$_POST['name'];//post获取表单里的name post to get the name in the form
-    $password=$_POST['password'];//post获取表单里的password
+    $password=$_POST['password'];//post获取表单里的password post to get the password in the form
+    $Level=$_POST['Level'];
 
-    include('Connect.php');//链接数据库post to get the password in the form
+
+    include('Connect.php');//链接数据库link to database
     $q="insert into user(id,username,password) values (null,'$name','$password')";//向数据库插入表单传来的值的sql
     $reslut=mysql_query($q,$con);//执行sql
     
