5fb38d9ac9fc42c141a7f07086e60e1e4cca0f11
Domain314 <artjom.moisejev@hotmail.com>
2022-01-07 00:17:08 +0100

    integrated desktop-dashboard
    - added users-dashboard
    - integrated in project
    - more navbar links in admin-mode

--- htdocs/php/sites/dashboard/load-desktop.php
+++ htdocs/php/sites/dashboard/load-desktop.php
@@ -5,6 +5,7 @@
     "<script>window.location.href='index.php?site=error';</script>";
   }
 
+  // get data from database, based on $_GET['view']
   function loadData($startColumn, $view) {
     include('php/utils/connect.php');
     if($stmt = $conn->prepare("SELECT * FROM ".$view." ORDER BY timestamp DESC LIMIT " . $startColumn . ",10")) {
@@ -13,32 +14,76 @@
     } else { /*echo "error";*/ }
   }
 
+  // Decide on displaying News/Tickets or User-Data
   $result = loadData(0, $view);
-  prepareDesktopTN($result);
+  if($view == 'personal_data') {
+    prepareDesktopU($result);
+  } else {
+    prepareDesktopTN($result);
+  }
 
+  // Prepare User-Dashboard
+  function prepareDesktopU($data)
+  {
+    $json_a = file_get_contents('php/sites/dashboard/uViewDesktop.json');
+    $a = explode("$", $json_a);
+    // console_log($a);
+    echo $a[1];
+    $counter = -1;
+    foreach ($data as $key) {
+      createRowU($key, $counter, $a);
+      $counter *= -1;
+    }
+    echo $a[11];
+  }
+
+  // Make rows out of database- and uViewDesktop-Fragments
+  function createRowU($row, $counter, $a) {
+    $username = getUserName($row[0]);
+    echo $a[2] . $counter;
+    echo $a[3] . $row[1] . $a[4] . $row[1] . $a[5];
+    echo $a[6] . $username . $a[7];
+    echo $a[6] . $row[3] . $a[7];
+    echo $a[6] . $row[4] . $a[7];
+    echo $a[6] . $row[10] . $a[7];
+    echo $a[6] . $row[5] . ' ' . $row[6] . $a[7];
+    echo $a[6] . $row[7] . ' ' . $row[8] . $a[7];
+    echo $a[8] . $row[9] . $a[9];
+  }
+
+  function getUserName($ID) {
+    include('php/utils/connect.php');
+    $stmt = $conn->prepare("SELECT username FROM user WHERE ID=".$ID);
+    $stmt->execute();
+    return $stmt->fetchColumn();
+  }
+
+  // Prepare TicketsNews-Dashboard
   function prepareDesktopTN($data) {
     $counter = 1;
     $json_a = file_get_contents('php/sites/dashboard/tnViewDesktop.json');
     $a = explode("$", $json_a);
-
+    // console_log($a);
     echo $a[1];
     foreach ($data as $key) {
-      createRow($key, $counter, $a);
+      if (isset($key[5])) {$key[5] = null;}
+      createRowTN($key, $counter, $a);
       $counter++;
     }
-    echo $a[12];
+    echo $a[13];
   }
 
-  function createRow($row, $counter, $a) {
-    echo $a[2] . $row[5] . $a[3] . $counter . $a[4] . $row[0] . $a[5] . $row[1] . $a[6];
+  // Make rows out of database- and tnViewDesktop-Fragments
+  function createRowTN($row, $counter, $a) {
+    $even = ($counter%2==0) ? '1' : '0';
+    echo $a[2] . $even . $a[3] . $row[5] . $a[4] . $counter . $a[5] . $row[0] . $a[6] . $row[1] . $a[7];
     if ($row[2] != '') {
       $pics = explode(",", $row[2]);
       foreach ($pics as $key) {
-        echo $a[7] . $key . $a[8];
+        echo $a[8] . $key . $a[9];
       }
     }
-    echo $a[9] . $row[3] . $a[10] . $row[4] . $a[11];
+    echo $a[10] . $row[3] . $a[11] . $row[4] . $a[12];
   }
-
 
  ?>
