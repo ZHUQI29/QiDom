1eaaef0a8dd0fbd53233ac8a11cde791a0a94fe0
ZHUQI29 <if21b206@technikum-wien.at>
2022-01-18 03:35:41 +0100

    logout
    Funktioniert!!!

--- htdocs/php/sites/logout.php
+++ htdocs/php/sites/logout.php
@@ -10,7 +10,9 @@ console_log(session_id());
 
 if(isset($_COOKIE[session_name()])){
      setcookie(session_name(),session_id(), time()-3600*24*30);
+     setcookie('level', 0, time()+3600*24);
+
 }
 session_destroy();
-// header("Location:./index.php");
+header("Location:./index.php");
 ?>
