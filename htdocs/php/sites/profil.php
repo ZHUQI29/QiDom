5041e1e36b2f7c0e483a43f9deb2d17d26258390
ZHUQI29 <if21b206@technikum-wien.at>
2022-01-18 12:48:02 +0100

    Profile
    Position von Profil geändert und Profil geändert

--- htdocs/php/sites/profil.php
+++ htdocs/php/sites/profil.php
@@ -3,6 +3,50 @@
   <form class="qd-bg-light reg-form-con d-lg-flex justify-content-center"name="change profile" action="db-registration.php?site=user" method="POST" enctype="multipart/form-data">
     <div class="d-flex flex-column align-items-center justify-content-between">
       <fieldset class="mx-3">
+      <legend>Profile</legend>
+      <?php
+    $server="localhost";
+    $db_username="root";
+    $db_password="";
+    $db_name="technikum-wsp";
+
+$conn = mysqli_connect($server, $db_username, $db_password, $db_name);
+if (isset($_GET['id'])) {
+  $id = $_GET['id'];
+} else {
+  // Get ID from user-table
+  $sql = "SELECT ID FROM user WHERE username='" . $_COOKIE['user'] . "'";
+  $result = mysqli_query($conn, $sql);
+  $id = mysqli_fetch_assoc($result)['ID'];
+}
+
+// console_log($id);
+echo "<input type='hidden' name='ID' value='" . $id . "'>";
+
+// Get info from personal_data-table
+$sql = "SELECT anrede, vorname, nachname, plz, ort, strasse, hausnummer, birthday, email FROM personal_data WHERE ID='" . $id . "'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // 输出数据
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "Anrede: " . $row["anrede"]. "<br>";
+        echo "Vorname: " . $row["vorname"]. "<br>";
+        echo "Nachname: " . $row["nachname"]. "<br>";
+        echo "PLZ: " . $row["plz"]. "<br>";
+        echo "Ort: " . $row["ort"]. "<br>";
+        echo "Straße: " . $row["strasse"]. "<br>";
+        echo "Hausnummer: " . $row["hausnummer"]. "<br>";
+        echo "Geburtstag: " . $row["birthday"]. "<br>";
+        echo "Email: " . $row["email"]. "<br>";
+        // echo "Photo: " . $row["photo_id"]. "<br>";
+    }
+} else {
+    echo "0 ";
+}
+mysqli_close($conn);
+?>
+        <br><br><br>
         <legend>Change profile</legend>
         <br>
         <label for="anrede">Anrede:</label>
@@ -57,45 +101,5 @@
       </fieldset>
 
 
-<?php
-    $server="localhost";
-    $db_username="root";
-    $db_password="";
-    $db_name="technikum-wsp";
 
-$conn = mysqli_connect($server, $db_username, $db_password, $db_name);
-if (isset($_GET['id'])) {
-  $id = $_GET['id'];
-} else {
-  // Get ID from user-table
-  $sql = "SELECT ID FROM user WHERE username='" . $_COOKIE['user'] . "'";
-  $result = mysqli_query($conn, $sql);
-  $id = mysqli_fetch_assoc($result)['ID'];
-}
-
-// console_log($id);
-echo "<input type='hidden' name='ID' value='" . $id . "'>";
-
-// Get info from personal_data-table
-$sql = "SELECT anrede, vorname, nachname, plz, ort, strasse, hausnummer, birthday, email FROM personal_data WHERE ID='" . $id . "'";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-    // 输出数据
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "Anrede: " . $row["anrede"]. "<br>";
-        echo "Vorname: " . $row["vorname"]. "<br>";
-        echo "Nachname: " . $row["nachname"]. "<br>";
-        echo "PLZ: " . $row["plz"]. "<br>";
-        echo "Ort: " . $row["ort"]. "<br>";
-        echo "Straße: " . $row["strasse"]. "<br>";
-        echo "Hausnummer: " . $row["hausnummer"]. "<br>";
-        echo "Geburtstag: " . $row["birthday"]. "<br>";
-        echo "Email: " . $row["email"]. "<br>";
-        // echo "Photo: " . $row["photo_id"]. "<br>";
-    }
-} else {
-    echo "0 ";
-}
-mysqli_close($conn);
-?>
+ 
